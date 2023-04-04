c401f8c55bc1733fbea81cc4518ba05afb9e4c8c
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-04-03 21:00:57 -0400

    finished

--- routes/routes.js
+++ routes/routes.js
@@ -19,16 +19,55 @@ router.get('/',(req,res)=>{
     res.sendFile(join(__dirname,'../index.html'))
 })
 
+router.get('/listado',async(req,res)=>{
+  const dbhandler = new DbHandler();
+  const arrUsuarios = await dbhandler.getUsuarios();
+  let htmlUsuarios = '';
+
+  arrUsuarios.forEach(el =>{
+    htmlUsuarios += `<tr><td>Email: ${el.email}</td> <td>Password: ${el.password}</td></tr>`
+  })
+
+  res.send(`<h1>Lista de Usuarios</h1><table id=tablausuarios>${htmlUsuarios}</table>`)
+})
+
+
 
 //===========================POST INGRESO=================================
 router.post('/usuario',async(req,res)=>{
     let mail = req.body.email;
     let pass = req.body.password;
 
-    const bdhandler = new DbHandler();
+    const dbhandler = new DbHandler();
 
-    await bdhandler.setUsuario(mail,pass);
+    await dbhandler.setUsuario(mail,pass);
     console.log('Este se deberia ejecutar despues');
+})
+
+router.post('/login',async(req,res)=>{
+  let mail = req.body.email;
+  let pass = req.body.password;
+
+  const dbhandler = new DbHandler();
+
+  let usuario = await dbhandler.getUsuariobyMail(mail);
+  console.log(usuario);
+  if (usuario){
+    let arrUsuarios = await dbhandler.getUsuarios();
+    let encontrado = arrUsuarios.find(e => e.email == mail);
+    let index = arrUsuarios.indexOf(encontrado);
+    
+    if (arrUsuarios[index].password == pass){
+      res.send('<script>alert("Usuario Logueado");window.location.href="/"</script>');
+    } else {
+      res.send('<script>alert("Contraseña Incorrecta");window.location.href="/"</script>');
+    }
+  } else {
+    res.send('<script>alert("Este mail no se encuentra registrado.");window.location.href="/"</script>');
+  }
+
+  
+  
 })
 
 
--- utils/class/DbHandler.js
+++ utils/class/DbHandler.js
@@ -10,8 +10,32 @@ export class DbHandler{
 
     async setUsuario(correo,pass){
         try{
-            const res = await pool.query('INSERT INTO usuarios (email, password) VALUES ($1,$2) RETURNING *',[correo,pass])
+            const res = await pool.query('INSERT INTO usuarios (email, password) VALUES ($1,$2) RETURNING *',[correo,pass]);
             console.log(`Email ${res.rows[0].email} registrado con éxito.`);
+        }
+        catch(e){
+            throw e;
+        }
+    }
+
+    async getUsuarios(){
+        try{
+            const res = await pool.query('SELECT * FROM usuarios'); 
+            return res.rows;
+        }
+        catch(e){
+            throw e;
+        }
+    }
+
+    async getUsuariobyMail(mail){
+        try{
+            const res = await pool.query('SELECT * FROM usuarios WHERE email = $1',[mail]);
+            if (res.rows[0] != undefined){
+                return res.rows[0];
+            } else {
+                return false;
+            }
         }
         catch(e){
             throw e;
